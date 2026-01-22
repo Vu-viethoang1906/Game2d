cce4295ee4fe7b3cbf2d6efa586b6426049f24e5
Vu-viethoang1906 <vuviethoang19062005@gmail.com>
2026-01-22 17:30:21 +0700

    CREATE AUTOMATIC SHOOTING

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,9 +4,12 @@ public class PlayerShooting : MonoBehaviour
 {
     public static PlayerShooting instance;
     
-    public GameObject bulletPrefab;
+    public GameObject bulletPrefabs;
+    public float shootingInterval = 0.5f;
     public int weaponPower = 1;
     public int maxweaponPower = 5;
+
+    private float lastBulletTime;
 
     void Awake()
     {
@@ -15,10 +18,16 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // Automatic shooting based on interval
+        if (Time.time - lastBulletTime > shootingInterval)
         {
-            Instantiate(bulletPrefab, transform.position,
-            transform.rotation);
+            ShootBullet();
+            lastBulletTime = Time.time;
         }
+    }
+
+    private void ShootBullet()
+    {
+        Instantiate(bulletPrefabs, transform.position, transform.rotation);
     }
 }

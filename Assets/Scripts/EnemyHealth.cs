2d2d45d3ac4c6d7e7a9588a6d649f406bbf4fbb4
Vu-viethoang1906 <vuviethoang19062005@gmail.com>
2026-01-28 01:34:22 +0700

    update EnemyHealth

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 
+
 public class EnemyHealth : MonoBehaviour
 {
     public GameObject explosionPrefab;
 
     private void OnTriggerEnter2D(Collider2D collision) => Die();
 
-    private void Die() => Destroy(gameObject);
+    private void Die()
+    {
+        var explosion = Instantiate(explosionPrefab, transform.position,
+        transform.rotation);
+        Destroy(explosion, 1);
+        Destroy(gameObject);
+    }
 }
